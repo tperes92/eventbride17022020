20e4c4987028c06c5a36aa3c096dea42068027cd
Thibault Peres <peresthibaul@gmail.com>
2020-02-18 15:19:26 +0100

    2nd day commit and pushing to Heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 require 'user_mailer'
 
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 after_create :welcome_send
 	has_many :attendances
 	has_many :events, through: :attendance
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_17_140156) do
+ActiveRecord::Schema.define(version: 2020_02_18_132049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,9 @@ ActiveRecord::Schema.define(version: 2020_02_17_140156) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
   end
 
 end
