ff41d1e3619370b2e494bdac731f4f547fa1879e
Thibault Peres <peresthibaul@gmail.com>
2020-02-18 13:09:33 +0100

    maj

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,5 +1,14 @@
+require 'attendance_mailer'
+
 class Attendance < ApplicationRecord
+after_create :welcome_send
+
 	belongs_to :user
 	belongs_to :event
 	validates :stripe_customer_id, uniqueness: true
+
+	def welcome_send
+	    AttendanceMailer.welcome_email(self).deliver_now
+	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
+require 'user_mailer'
+
 class User < ApplicationRecord
+after_create :welcome_send
 	has_many :attendances
 	has_many :events, through: :attendance
 	has_many :admin_events, foreign_key: 'admin_id', class_name: "Event"
+
+def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+end
+
 end
